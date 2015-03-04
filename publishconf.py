da00162b1d5102ebe47d213140dc37cec23b1eac
Panos Mavrogiorgos <pmav99@gmail.com>
2015-03-04 18:51:08 +0200

    final hashover?

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://logiconomy.com'
+SITEURL = 'http://logiconomy.gr'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
